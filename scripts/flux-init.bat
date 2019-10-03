a97a9e3e23739e7b643751a43ec80f4b7a33185f
Jeff Wenzbauer <jeff.wenzbauer@incontact.com>
2019-10-03 08:47:13 -0600

    Update flux-init.bat

--- scripts/flux-init.bat
+++ scripts/flux-init.bat
@@ -1,3 +1,5 @@
+helm repo add fluxcd https://charts.fluxcd.io
+
 helm upgrade -i flux --wait --set git.url=git@github.com:jwenz723/kubernetes-workshop-flux --set git.branch=master --set git.path=k8s/ --set git.pollInterval=15s --set registry.pollInterval=15s --set helmOperator.chartsSyncInterval=15s --set syncGarbageCollection.enabled=true --namespace flux -f flux-values.yaml fluxcd/flux
 
 kubectl -n flux rollout status deployment/flux
