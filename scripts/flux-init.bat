23a1c86b7c8d540ba205477104c82fe65c3c19a1
Pablo Sangueza <pablo.sangueza@niceincontact.com>
2019-10-03 12:12:42 -0400

    updated docker image path

--- scripts/flux-init.bat
+++ scripts/flux-init.bat
@@ -1,6 +1,6 @@
 helm repo add fluxcd https://charts.fluxcd.io
 
-helm upgrade -i flux --wait --set git.url=git@github.com:jwenz723/kubernetes-workshop-flux --set git.branch=master --set git.path=k8s/ --set git.pollInterval=15s --set registry.pollInterval=15s --set helmOperator.chartsSyncInterval=15s --set syncGarbageCollection.enabled=true --namespace flux -f flux-values.yaml fluxcd/flux
+helm upgrade -i flux --wait --set git.url=git@github.com:pablosangueza/kubernetes-workshop-flux --set git.branch=master --set git.path=k8s/ --set git.pollInterval=15s --set registry.pollInterval=15s --set helmOperator.chartsSyncInterval=15s --set syncGarbageCollection.enabled=true --namespace flux -f flux-values.yaml fluxcd/flux
 
 kubectl -n flux rollout status deployment/flux
 kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2
